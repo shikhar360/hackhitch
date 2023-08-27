561285ab53334a3f67c97e0beb14a7760871c987
Kamal Nayan <95926324+kamalbuilds@users.noreply.github.com>
2023-08-28 03:39:55 +0530

    gql bug fixed

--- src/app/api.ts
+++ src/app/api.ts
@@ -32,95 +32,95 @@ query ExploreProfiles {
 }
 `
 
-// export const getMyProfileEth2 = (addr : string)=>{
+export const getMyProfileEth2 = (addr : string)=>{
 
-// const x = gql`
-// query DefaultProfile {
-//   defaultProfile(request: { ethereumAddress: ${addr}}) {
-//     id
-//     name
-//     bio
-//     isDefault
-//     attributes {
-//       displayType
-//       traitType
-//       key
-//       value
-//     }
-//     followNftAddress
-//     metadata
-//     handle
-//     picture {
-//       ... on NftImage {
-//         contractAddress
-//         tokenId
-//         uri
-//         chainId
-//         verified
-//       }
-//       ... on MediaSet {
-//         original {
-//           url
-//           mimeType
-//         }
-//       }
-//     }
-//     coverPicture {
-//       ... on NftImage {
-//         contractAddress
-//         tokenId
-//         uri
-//         chainId
-//         verified
-//       }
-//       ... on MediaSet {
-//         original {
-//           url
-//           mimeType
-//         }
-//       }
-//     }
-//     ownedBy
-//     dispatcher {
-//       address
-//       canUseRelay
-//     }
-//     stats {
-//       totalFollowers
-//       totalFollowing
-//       totalPosts
-//       totalComments
-//       totalMirrors
-//       totalPublications
-//       totalCollects
-//     }
-//     followModule {
-//       ... on FeeFollowModuleSettings {
-//         type
-//         contractAddress
-//         amount {
-//           asset {
-//             name
-//             symbol
-//             decimals
-//             address
-//           }
-//           value
-//         }
-//         recipient
-//       }
-//       ... on ProfileFollowModuleSettings {
-//        type
-//       }
-//       ... on RevertFollowModuleSettings {
-//        type
-//       }
-//     }
-//   }
-// }
-// `
-// return x
-// }
+  return  gql`
+  query DefaultProfile($ethereumAddress: String!) {
+    defaultProfile(request: { ethereumAddress: $ethereumAddress }) {
+      id
+      name
+      bio
+      isDefault
+      attributes {
+        displayType
+        traitType
+        key
+        value
+      }
+      followNftAddress
+      metadata
+      handle
+      picture {
+        ... on NftImage {
+          contractAddress
+          tokenId
+          uri
+          chainId
+          verified
+        }
+        ... on MediaSet {
+          original {
+            url
+            mimeType
+          }
+        }
+      }
+      coverPicture {
+        ... on NftImage {
+          contractAddress
+          tokenId
+          uri
+          chainId
+          verified
+        }
+        ... on MediaSet {
+          original {
+            url
+            mimeType
+          }
+        }
+      }
+      ownedBy
+      dispatcher {
+        address
+        canUseRelay
+      }
+      stats {
+        totalFollowers
+        totalFollowing
+        totalPosts
+        totalComments
+        totalMirrors
+        totalPublications
+        totalCollects
+      }
+      followModule {
+        ... on FeeFollowModuleSettings {
+          type
+          contractAddress
+          amount {
+            asset {
+              name
+              symbol
+              decimals
+              address
+            }
+            value
+          }
+          recipient
+        }
+        ... on ProfileFollowModuleSettings {
+        type
+        }
+        ... on RevertFollowModuleSettings {
+        type
+        }
+      }
+    }
+  }
+  `
+}
+
 export const getMyProfileEth = gql`
 query DefaultProfile {
   defaultProfile(request: { ethereumAddress: "0x15acfD6c3C7Ca01Fc3a11C6cB3155377984305f2"}) {
